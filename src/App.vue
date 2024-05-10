ab829b4574c04377e2f44f044969a00008dc3759
Yahor Yankavets <mr.yankovec@gmail.com>
2024-05-10 23:49:36 +0300

    added header component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import TheHeader from './components/TheHeader.vue'
+</script>
 
 <template>
   <div class="bg-white h-screen w-4/5 m-auto rounded-xl shadow-2xl mt-14">
+    <TheHeader />
+
     <h1>vue-sneakers</h1>
   </div>
 </template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="flex justify-between border-b-2 border-slate-300 p-8">
+    <div class="flex items-center gap-4">
+      <img class="w-10" src="/logo.png" alt="Logo" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-500">This is the best sneaker store</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-2 cursor-pointer text-slate-500 hover:text-black">
+        <img src="/cart.svg" alt="basket" />
+        <span>70$</span>
+      </li>
+
+      <li class="flex items-center gap-2 cursor-pointer text-slate-500 hover:text-black">
+        <img src="/heart.svg" alt="favorites" />
+        <span>Favorites</span>
+      </li>
+
+      <li class="flex items-center gap-2 cursor-pointer text-slate-500 hover:text-black">
+        <img src="/profile.svg" alt="profile" />
+        <span>Profile</span>
+      </li>
+    </ul>
+  </header>
+</template>
